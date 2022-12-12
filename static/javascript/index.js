159083b38b2be003aa87cd5ea4f3c19e0e7acde5
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2022-12-13 00:29:56 +0100

    added faq section

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,4 +1,15 @@
-//color of four links to change on click?
+//color of four links to change on click
 document.querySelector(".links").onclick=ev=>{if(ev.target.tagName=="A")
   ev.target.className="done"
 }
+
+
+//to toggle faqs
+
+const faq = document.querySelectorAll(".faqs")
+
+faq.forEach((faqs) => {
+  faqs.addEventListener("click", () => {
+    faqs.classList.toggle("active");
+  });
+});
